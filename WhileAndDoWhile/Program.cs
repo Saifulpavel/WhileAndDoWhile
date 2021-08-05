1565a686fc7727dd39d1457c91b10443dd905577
saiful Islam <saifulislam0980@gmail.com>
2021-08-06 02:33:31 +0600

    Created

--- WhileAndDoWhile/Program.cs
+++ WhileAndDoWhile/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WhileAndDoWhile
 {
@@ -10,6 +6,46 @@ namespace WhileAndDoWhile
     {
         static void Main(string[] args)
         {
+            //While and Do While Loop// Break statement
+            int i = 0;
+            while (true)
+            {
+                if (i > 10)
+                {
+                    break;
+                    
+                }
+                
+                Console.WriteLine(i);
+                i++;
+            }
+
+            //Continue statement
+            int j = 0;
+            while (j < 20)
+            {
+                j++;
+                if (j%5==0)
+                {
+                    continue;
+                }
+                Console.WriteLine(j);
+            }
+
+
+            //Do While Loop
+
+            int k = 0;
+
+            do
+            {
+                Console.WriteLine("Value of k: {0}", k);
+
+                k++;
+
+            } while (k < 10);
+
+            Console.ReadKey();
         }
     }
 }
